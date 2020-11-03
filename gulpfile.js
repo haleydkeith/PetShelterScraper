1d8c897a278dd2cc549f7689ab3c8e4fbc068d45
Haley D. Keith <64813404+haleydkeith@users.noreply.github.com>
2020-11-03 13:44:58 -0600

    gulp2

--- gulpfile.js
+++
@@ -1,5 +0,0 @@
-gulp.task('watch', gulp.series(['browserSync', 'sass', 'scripts'], function () {
-  gulp.watch('main/*.html', gulp.series(reload));  
-  gulp.watch('main/assets/css/**/*.scss', gulp.series(['sass']));
-  gulp.watch('main/assets/js/components/*.js', gulp.series(['scripts']));
-}));
